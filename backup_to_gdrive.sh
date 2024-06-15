571305269b56e7efbc31e2377f788667489cc4ed
Miracle Modey <miraclemodey@Miracles-MacBook-Air.local>
2024-06-14 20:01:29 -0400

    Updated clean up date to older than 3 months

--- backup_to_gdrive.sh
+++ backup_to_gdrive.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 #Configuration
-SOURCE_DIR=""                   # Directory to back up
-BACKUP_DIR=""                   # Directory to store the initial backup file  
-ENCRYPTED_BACKUP=""             # Directoru to store the encrypted backup file
-LOG_FILE=""                     # Log file to record backup operations
-REMOTE="google_drive:backup"    # rclone remote configuration for Google Drive
-KEY_FILE=""                     # File containing the encryption key
+SOURCE_DIR="/path/to/source/directory"                  # Directory to back up
+BACKUP_DIR="/path/to/backup/directory"                  # Directory to store the initial backup file  
+ENCRYPTED_BACKUP="/path/to/encripted/directory"         # Directoru to store the encrypted backup file
+LOG_FILE="/path/to/log/file"                            # Log file to record backup operations
+REMOTE="google_drive:backup"                            # rclone remote configuration for Google Drive
+KEY_FILE="/path/to/key/file"                            # File containing the encryption key
 
 # Create a backup archive 
 tar -czf ${BACKUP_DIR}/backup_$(date +%F).tar.gz -C ${SOURCE_DIR} .
@@ -20,6 +20,6 @@ rclone copy ${ENCRYPTED_BACKUP}/backup_$(date +%F).tar.gz.enc ${REMOTE}
 # Log the operation
 echo "Backup and transfer completed on $(date)" >> ${LOG_FILE}
 
-# Clean up local backup files older than 7 days
-find ${BACKUP_DIR} -type f -mtime +7 -exec rm  {} \;
-find ${ENCRYPTED_BACKUP} -type f -mtime +7 -exec rm {} \;
+# Clean up local backup files older than 3 months
+find ${BACKUP_DIR} -type f -mtime +90 -exec rm  {} \;
+find ${ENCRYPTED_BACKUP} -type f -mtime +90 -exec rm {} \;
